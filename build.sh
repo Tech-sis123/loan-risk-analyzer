d76f37bf0fe9f4a639949e492eef38030d5aaa1d
Tech-sis123 <esabublessing7@gmail.com>
2025-06-28 22:01:14 +0100

    Remove .venv from repo and ignore it

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ python -m venv .venv
 source .venv/bin/activate
 
 # Install build essentials
-pip install --upgrade pip setuptools wheel
+#pip install --upgrade pip setuptools wheel
 
 # Install requirements
 pip install -r requirements.txt
