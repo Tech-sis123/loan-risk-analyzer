14523a9446e727135fc0aa1704370b1490af7dce
Tech-sis123 <esabublessing7@gmail.com>
2025-06-29 08:30:25 +0100

    eigth commit

--- app.py
+++ app.py
@@ -60,5 +60,6 @@ def predict():
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+
