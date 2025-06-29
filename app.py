73f5f71b25fef5d7e8312422cb5164b7ad64fe67
Tech-sis123 <esabublessing7@gmail.com>
2025-06-29 07:36:41 +0100

    Dockerfile added

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template, jsonify
 import pandas as pd
 import joblib
+import os
 from werkzeug.exceptions import BadRequest
 
 app = Flask(__name__)
@@ -58,5 +59,5 @@ def predict():
     
     return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+port = int(os.environ.get("PORT", 8000))
+app.run(host="0.0.0.0", port=port)
